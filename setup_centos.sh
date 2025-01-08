1c6ecd2da6c907e66f9f7b2d341429c44cf9f4f3
Qing Shuai <s_q@zju.edu.cn>
2025-01-08 11:22:57 +0800

    Create setup_centos.sh

---
+++ setup_centos.sh
@@ -0,0 +1,5 @@
+yum install tmux wget curl
+yum install python
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
