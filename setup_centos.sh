355f0780c185f8fd4a0e87847b3e5727a0805b13
Qing Shuai <s_q@zju.edu.cn>
2025-01-10 10:43:34 +0800

    Update setup_centos.sh

--- setup_centos.sh
+++ setup_centos.sh
@@ -1,5 +1,13 @@
 yum install tmux wget curl
-yum install python
+yum install python python3-pip
+yum install ffmpeg
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+yum install 
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip3 install torch openai-whisper
+pip3 config set global.index-url http://mirrors.cloud.tencent.com/pypi/simple
+pip3 config set global.trusted-host mirrors.cloud.tencent.com
